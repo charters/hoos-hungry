ae35c994538d1d717148406274c7183d07deb8ed
Alexander Charters <aac8mf@virginia.edu>
2017-03-15 20:59:42 -0700

    First commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+//Libraries
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//Component
+import App from './App';
+
+//Style
+import './index.css';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
